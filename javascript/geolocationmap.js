895d33c578924510384e5e75a8260f79807eeddf
eighty-nine89 <awueteysamuel222@yahoo.com>
2024-07-13 23:44:33 +0000

    update-001

--- javascript/geolocationmap.js
+++ javascript/geolocationmap.js
@@ -1,110 +1,92 @@
 class Geolocation {
-    // on success
-    successCallback(position) {
-        let result = document.querySelector("#result") // get the result div
-        result.style.display = "block" // show the result div
-        // result.innerText = "User's Cordinates: " + "Lat: " + position.coords.latitude + ", Long: " + position.coords.longitude // display the latitude and longitude
-        let mapContainer = document.querySelector("#map") // get the map container
-        mapContainer.style.display = "block" // show the map container
-        const map = L.map("map").setView(
-            [position.coords.latitude, position.coords.longitude],
-            14.5
-        )
-        //using a red marker map icon for user position
-        // var myIcon = L.icon({
-        //     iconUrl: 'images/iconmarkermap1.png',
-        //     iconSize: [50, 40],
-        //     iconAnchor: null,
-        // });
-        // create a map and set the view to the user's location
-        const tiles = L.tileLayer(
-            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            {
-                maxZoom: 19,
-                attribution:
-                    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }
-        ).addTo(map) // add the tiles to the map
-        
-        // adding map marker to the map
-        L.marker([6.673164749452684, -1.5674784750938933]).addTo(map)
-            .bindTooltip('College of Science, KNUST. (Food Bank)').openTooltip()
-            .on("click", ()=>{window.open("COSfoodbank.html", "_blank")});
+  // on success
+  successCallback(position) {
+    let result = document.querySelector("#result"); // get the result div
+    result.style.display = "block"; // show the result div
 
-        // adding map marker to the map
-        L.marker([6.686903051808014, -1.566822804204397]).addTo(map)
-            .bindTooltip('Community Center, KNUST. (Food Bank)').openTooltip()
-            .on("click", ()=>{window.open("COMCENfoodbank.html", "_blank")});
-        // adding map marker to the map
-        L.marker([6.675465398222645, -1.5667430679775691]).addTo(map)
-            .bindTooltip('College of Agriculture, KNUST. (Food Bank)').openTooltip()
-            .on("click", ()=>{window.open("AGRICfoodbank.html", "_blank")});
-        // adding map marker to the map
-        L.marker([6.682683444352829, -1.5760730753687446]).addTo(map)
-            .bindTooltip('Food Bank center, KNUST. (Food Bank)').openTooltip()
-            .on("click", ()=>{window.open("FOODBANK.html", "_blank")});        
-        // adding map marker to the map
-        L.marker([6.6691961538699625, -1.5744769654935014]).addTo(map)
-            .bindTooltip('Brunei Complex, KNUST. (Food Bank)').openTooltip()
-            .on("click", ()=>{window.open("BRUNIEfoodbank.html", "_blank")});         
-        // user default position on the map
-        const marker = L.marker([
-            position.coords.latitude,
-            position.coords.longitude
-        ]).addTo(map) // add a marker to the map
-            .bindPopup('Your Current Location')
-            .openPopup();
+    let mapContainer = document.querySelector("#map"); // get the map container
+    mapContainer.style.display = "block"; // show the map container
+    const map = L.map("map").setView(
+      [position.coords.latitude, position.coords.longitude],
+      14.5
+    );
 
-        // when a red marker icon is for user position
-        // var myIcon = L.icon({
-        //     iconUrl: 'images/marker.png',
-        //     iconSize: [38, 95],
-        //     iconAnchor: [22, 94],
-        //     popupAnchor: [-3, -76],
-        // });
-        // const marker = L.marker([
-        //     position.coords.latitude,
-        //     position.coords.longitude
-        // ], { icon: myIcon }).addTo(map) // add a marker to the map
-        //     .bindPopup('User Position')
-        //     .openPopup();
+    const tiles = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }
+    ).addTo(map); // add the tiles to the map
 
-        // marker.addEventListener("click", function(){ alert("Your current location."); });
+    // adding map marker to the map
+    L.marker([6.673164749452684, -1.5674784750938933]).addTo(map)
+      .bindTooltip('College of Science, KNUST. (Food Bank)').openTooltip()
+      .on("click", () => { window.open("COSfoodbank.html", "_blank"); });
+
+    // adding map marker to the map
+    L.marker([6.686903051808014, -1.566822804204397]).addTo(map)
+      .bindTooltip('Community Center, KNUST. (Food Bank)').openTooltip()
+      .on("click", () => { window.open("COMCENfoodbank.html", "_blank"); });
+
+    // adding map marker to the map
+    L.marker([6.675465398222645, -1.5667430679775691]).addTo(map)
+      .bindTooltip('College of Agriculture, KNUST. (Food Bank)').openTooltip()
+      .on("click", () => { window.open("AGRICfoodbank.html", "_blank"); });
+
+    // adding map marker to the map
+    L.marker([6.682683444352829, -1.5760730753687446]).addTo(map)
+      .bindTooltip('Food Bank center, KNUST. (Food Bank)').openTooltip()
+      .on("click", () => { window.open("FOODBANK.html", "_blank"); });
+
+    // adding map marker to the map
+    L.marker([6.6691961538699625, -1.5744769654935014]).addTo(map)
+      .bindTooltip('Brunei Complex, KNUST. (Food Bank)').openTooltip()
+      .on("click", () => { window.open("BRUNIEfoodbank.html", "_blank"); });
+
+    // user default position on the map
+    const marker = L.marker([
+      position.coords.latitude,
+      position.coords.longitude
+    ]).addTo(map) // add a marker to the map
+      .bindPopup('Your Current Location')
+      .openPopup();
+  }
+
+  // on error
+  errorCallback(error) {
+    let result = document.querySelector("#result"); // get the result div
+    result.style.display = "block"; // show the result div
+    if (error.code == 1) { // if the user denied the request
+      result.innerText = "You have not given permission to access your location.";
+    } else if (error.code == 2) { // if the position is unavailable
+      result.innerText = "Your location is unavailable.";
+    } else if (error.code == 3) { // if the request times out
+      result.innerText = "The request to get your location timed out.";
+    } else { // if something else went wrong
+      result.innerText = "An unknown error occurred.";
     }
-    // on error
-    errorCallback(error) {
-        let result = document.querySelector("#result") // get the result div
-        result.style.display = "block" // show the result div
-        if (error.code == 1) { // if the user denied the request
-            result.innerText = "You have not given permission to access your location."
-        } else if (error.code == 2) { // if the position is unavailable
-            result.innerText = "Your location is unavailable."
-        } else if (error.code == 3) { // if the request times out
-            result.innerText = "The request to get your location timed out."
-        } else { // if something else went wrong
-            result.innerText = "An unknown error occurred."
-        }
+  }
+
+  showPosition() {
+    if (navigator.geolocation) { // if the browser supports geolocation
+      navigator.geolocation.getCurrentPosition(
+        this.successCallback,
+        this.errorCallback
+      ); // get the user's location
+      let result = document.querySelector("#result");
+      result.style.display = "block";
+    } else {
+      alert('Your browser does not support geolocation'); // if the browser doesn't support geolocation
     }
-    showPosition() {
-        if (navigator.geolocation) { // if the browser supports geolocation
-            navigator.geolocation.getCurrentPosition(
-                this.successCallback,
-                this.errorCallback
-            ) // get the user's location
-            let result = document.querySelector("#result")
-            result.style.display = "block"
-            // result.innerText = "Getting user position information..."
-        } else {
-            alert('Your browser does not support geolocation') // if the browser doesn't support geolocation
-        }
-    }
+  }
 }
-// const showPosition = document.querySelector("#showPosition")
-// showPosition.addEventListener("click", function (e) {
-//     e.preventDefault()
-//     let result = document.querySelector("#result")
-//     result.style.display = "block"
-//     new Geolocation().showPosition() // show the user's location
-// })
- 
 
+// Uncomment the following lines if you have a button to trigger showPosition
+// const showPosition = document.querySelector("#showPosition");
+// showPosition.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     let result = document.querySelector("#result");
+//     result.style.display = "block";
+//     new Geolocation().showPosition(); // show the user's location
+// });
